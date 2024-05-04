abc81ebba5ae20d36d254e8f3c07d0cd52302d3e
Anastasia R <anastasiarudakova@Anastasias-MacBook-Pro.local>
2024-05-04 20:52:32 +0800

    chore:add new file

--- Company.java
+++ Company.java
@@ -1,4 +1,4 @@
-import empoyees.Employee;
+import employees.Employee;
 import employees.Manager;
 import employees.Worker;
 
@@ -11,6 +11,14 @@ public class Company {
         employees[2] = new Manager("Jacob White", 7000, 2);
         employees[3] = new Worker("Andru Krown", 5500, "Marketing specialist");
         employees[4] = new Worker("Jan Brown", 6500, "Business coach");
+        int nonManagerCount = 0;
+        for (Employee employee : employees) {
+            if (!(employee instanceof Manager)) {
+                nonManagerCount++;
+            }
+        }
+        ((Manager) employees[0]).setNumberOfSubordinates(nonManagerCount);
+        employees[0].setSalary(7500);
         System.out.println("Dane pracownika o indeksie 3:");
         System.out.println(employees[3].toString());
         employees[3].setSalary(60000);
