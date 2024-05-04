c5b8580c73d82337cd5d1f4cdc5c4136ad92176b
Anastasia R <anastasiarudakova@Anastasias-MacBook-Pro.local>
2024-05-04 20:33:16 +0800

    chore:add new file

--- Company.java
+++ Company.java
@@ -1,11 +1,16 @@
+import empoyees.Employee;
+import employees.Manager;
+import employees.Worker;
+
+
 public class Company {
     public static void Main(String[] args); 
     Employee[] employees = new Employee[5];
-    employees[0] = new Employee("Anna Lee", 2000);
-        employees[1] = new Employee("Carl Bas", 6000);
-        employees[2] = new Employee("Jacob White", 7000);
-        employees[3] = new Employee("Andru Krown", 5500);
-        employees[4] = new Employee("Jan Brown", 6500);
+    employees[0] = new Manager("Anna Lee", 2000, 0);
+        employees[1] = new Worker("Carl Bas", 6000, "Director");
+        employees[2] = new Manager("Jacob White", 7000, 2);
+        employees[3] = new Worker("Andru Krown", 5500, "Marketing specialist");
+        employees[4] = new Worker("Jan Brown", 6500, "Business coach");
         System.out.println("Dane pracownika o indeksie 3:");
         System.out.println(employees[3].toString());
         employees[3].setSalary(60000);
